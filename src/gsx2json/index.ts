86a6dec386565c4feb6bd6fe507ee79b2fd5e860
Mirco Kraenz <kraenzmirco@gmail.com>
2021-08-16 09:35:02 +0200

    FIX #529 fetch spreadsheet  as csv and convert to json
    Reason: Google shut down the json endpoint. See https://twitter.com/MKraenz/status/1427164596306124801 for updates

--- src/gsx2json/index.ts
+++ src/gsx2json/index.ts
@@ -1,72 +1,29 @@
 import { Handler } from "aws-lambda";
 import Axios from "axios";
+import csvTojson from "csvtojson";
 import { respond } from "../utils/respond";
-import { googleSheetToJson } from "./googleSheetToJson";
-import { IQueryParams } from "./IQueryParams";
+import { assertEnvVar } from "../validation/assert";
 
 export const handler: Handler<{
-    queryStringParameters?: IQueryParams;
     httpMethod: "GET";
-}> = async (event, context, callback) => {
-    let options: ReturnType<typeof getOptions>;
+}> = async () => {
     try {
-        options = getOptions(event.queryStringParameters);
-    } catch (error) {
-        return respond(400, error);
-    }
-    try {
-        const spreadsheetData = await fetchSpreadsheetData(options);
-        const spreadsheetDataAsJson = googleSheetToJson(
-            options,
-            spreadsheetData
+        const spreadsheetPublicId = process.env.MESSAGES_SPREADSHEET_PUBLIC_ID;
+        assertEnvVar(spreadsheetPublicId, "MESSAGES_SPREADSHEET_PUBLIC_ID");
+        const spreadsheetAsCsv = await fetchSpreadsheetData(
+            spreadsheetPublicId
         );
-        return respond(200, spreadsheetDataAsJson);
+        const spreadsheetAsJson = await csvTojson({
+            trim: true,
+        }).fromString(spreadsheetAsCsv);
+        return respond(200, spreadsheetAsJson);
     } catch (error) {
         return respond(500, error);
     }
 };
 
-const fetchSpreadsheetData = async ({
-    id,
-    sheet,
-}: {
-    id: string;
-    sheet: number;
-}) => {
-    const url = `https://spreadsheets.google.com/feeds/list/${id}/${sheet}/public/values?alt=json`;
-    const res = await Axios.get<{ feed: { entry: any } }>(url);
+const fetchSpreadsheetData = async (spreadsheetPublicId: string) => {
+    const url = `https://docs.google.com/spreadsheets/d/e/${spreadsheetPublicId}/pub?gid=0&single=true&output=csv`;
+    const res = await Axios.get<string>(url);
     return res.data;
-};
-
-const getOptions = (
-    params?: IQueryParams
-): {
-    id: string;
-    sheet: number;
-    query: string;
-    useIntegers: boolean;
-    showRows: boolean;
-    showColumns: boolean;
-} => {
-    if (!params || !params.id)
-        throw new Error("id query parameter must be provided");
-    const id = params.id;
-    const sheet = Number.parseInt(params.sheet || "1", 10) || 1;
-    const query = params.q || "";
-    const useIntegers = params.integers ? !(params.integers === "false") : true;
-    const showRows = params.rows ? !(params.rows === "false") : true;
-    const showColumns = params.columns ? !(params.columns === "false") : true;
-    if (!showRows && !showColumns) {
-        throw new Error(
-            "Do not set BOTH rows=false and columns=false at the same time"
-        );
-    }
-    return {
-        id,
-        sheet,
-        query,
-        useIntegers,
-        showRows,
-        showColumns,
-    };
 };
