c24c30ac542e96e861e64b514f4f9a6e94dfda1d
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-03 22:44:01 +0200

    REFACTOR #535 delete dead code

--- src/push-notifications/TicketRepository.ts
+++ src/push-notifications/TicketRepository.ts
@@ -75,37 +75,6 @@ export class TicketRepository {
         return this.getMany("SuccessTicket");
     }
 
-    // TODO #535 delete
-    async deleteManySuccessTickets(uuids: string[]) {
-        if (uuids.length === 0) {
-            this.logger.log({ msg: "No tickets to delete" });
-            return;
-        }
-        if (uuids.length > MAX_DYNAMO_DB_BATCH_SIZE) {
-            throw new Error(
-                `Can only delete <=25 items at a time. uuids: ${uuids.join(
-                    ", "
-                )}`
-            );
-        }
-        this.logger.log({ msg: "start removing tickets", uuids });
-        await this.dynamoDb
-            .batchWrite({
-                RequestItems: {
-                    [this.tableName]: uuids.map(uuid => ({
-                        DeleteRequest: {
-                            Key: {
-                                type: "SuccessTicket",
-                                uuid,
-                            },
-                        },
-                    })),
-                },
-            })
-            .promise();
-        this.logger.log({ msg: "successfully removed tickets", uuids });
-    }
-
     async deleteMany(uuids: string[], type: Ticket["type"]) {
         if (uuids.length === 0) {
             this.logger.log({ msg: "No tickets to delete" });
