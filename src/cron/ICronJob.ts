e6a604f040ff1069e6aecd388220132f85fe979d
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-04 10:36:46 +0200

    ADD #535 migration script for webcron to aws

--- src/cron/ICronJob.ts
+++ src/cron/ICronJob.ts
@@ -1,4 +1,8 @@
 export interface ICronJob {
     cron_job_id: string;
     cron_job_name: string;
+    /** stringified {token: string} */
+    http_message_body: string;
+    /** example: '0 9 * * *' for 09:00 */
+    cron_expression: string;
 }
